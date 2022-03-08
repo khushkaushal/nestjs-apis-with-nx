3481ef62ced816730917f3eb8a628da1b49aaf46
Khush Kaushal <khushkaushal@Khushs-MacBook-Pro.local>
2022-03-07 23:07:10 -0800

    Implemented a Redis DB for backend using ioredis

--- apps/avengers/src/avengers/avengers.service.ts
+++ apps/avengers/src/avengers/avengers.service.ts
@@ -3,30 +3,35 @@ import { CreateAvengerDto } from './dto/create-avenger.dto'
 import { UpdateAvengerDto } from './dto/update-avenger.dto'
 import { Avenger } from './entities/avenger.entity'
 import { v4 as uuid } from 'uuid';
+import { RedisCacheService } from '../redis-cache/redis-cache.service';
 
 @Injectable()
 export class AvengersService {
 
+  constructor(private cacheManager: RedisCacheService) {}
+
   private avengers: Avenger[] = [];
 
-  create(createAvengerDto: CreateAvengerDto) {
+  async create(createAvengerDto: CreateAvengerDto) {
     const Id = uuid();
     const newAvenger = new Avenger(Id, createAvengerDto.name, createAvengerDto.strength, createAvengerDto.ability);
     this.avengers.push(newAvenger);
+    await this.cacheManager.set(Id, JSON.stringify(newAvenger));
     return `${createAvengerDto.name} added to avengers with ID: ${Id}`
   }
 
-  findAll() {
-    return {...this.avengers}
+  async findAll() {
+    const values = await this.cacheManager.getAllValues();
+    return (values);
   }
 
-  findOne(id: string) {
-    const avenger = this.findAvenger(id)[0]
-    return {...avenger};
+  async findOne(id: string) {
+    const value = await this.cacheManager.get(id);
+    return JSON.parse(value);
   }
 
-  update(id: string, updateAvengerDto: UpdateAvengerDto) {
-    const [avenger, index] = this.findAvenger(id);
+  async update(id: string, updateAvengerDto: UpdateAvengerDto) {
+    const avenger = JSON.parse(await this.cacheManager.get(id));
     const updatedAvenger = {...avenger};
     if (updateAvengerDto.name) {
       updatedAvenger.name = updateAvengerDto.name;
@@ -37,22 +42,14 @@ export class AvengersService {
     if (updateAvengerDto.ability) {
       updatedAvenger.ability = updateAvengerDto.ability;
     }
-    this.avengers[index] = updatedAvenger;
+    await this.cacheManager.set(id, JSON.stringify(updatedAvenger));
     return `${id} avenger updated`
   }
 
-  remove(id: string) {
-    const [avenger, index] = this.findAvenger(id);
-    this.avengers.splice(index, 0);
+  async remove(id: string) {
+    const avenger = JSON.parse(await this.cacheManager.get(id));
+    await this.cacheManager.del(id);
     return `${avenger.name} removed from the Avengers`
   }
 
-  private findAvenger(id: string): [Avenger, number] {
-    const avengerIndex = this.avengers.findIndex((avenger) => (avenger.id = id));
-    const avenger = this.avengers[avengerIndex];
-    if (!avenger) {
-      throw new NotFoundException();
-    }
-    return [avenger, avengerIndex];
-  }
 }
